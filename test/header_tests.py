b64606e12d0ce945ae928934a61b8f87066387d8
DJSchaffner <jonas.wilkens@outlook.com>
2025-05-23 15:40:58 +0200

    Add tests for header

--- test/header_tests.py
+++ test/header_tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 from src.header import Header
-from sample_data import get_header_compressed
+from sample_data import get_header_compressed, get_header_fabricated, get_header_uncompressed
 
 
 class HeaderTests(unittest.TestCase):
@@ -14,13 +14,63 @@ class HeaderTests(unittest.TestCase):
 
         # Assert
         self.assertIsNotNone(header)
+        self.assertIsNotNone(header.rec_version)
+        self.assertIsNotNone(header.checker)
+        self.assertIsNotNone(header.version_minor)
+        self.assertIsNotNone(header.version_major)
+        self.assertIsNotNone(header.build)
+        self.assertIsNotNone(header.timestamp)
+        self.assertIsNotNone(header.version)
+        self.assertIsNotNone(header.internal_version)
+        self.assertIsNotNone(header.data)
 
     def test_parse_with_uncompressed_data_returns_success(self):
         # Arrange
-        data = get_header_compressed()
+        data = get_header_uncompressed()
 
         # Act
-        header = Header.parse(data, is_compressed=True)
+        header = Header.parse(data, is_compressed=False)
 
         # Assert
         self.assertIsNotNone(header)
+        self.assertIsNotNone(header.rec_version)
+        self.assertIsNotNone(header.checker)
+        self.assertIsNotNone(header.version_minor)
+        self.assertIsNotNone(header.version_major)
+        self.assertIsNotNone(header.build)
+        self.assertIsNotNone(header.timestamp)
+        self.assertIsNotNone(header.version)
+        self.assertIsNotNone(header.internal_version)
+        self.assertIsNotNone(header.data)
+
+    def test_parse_should_read_data_properly(self):
+        # Arrange
+        data = get_header_fabricated()
+
+        # Act
+        header = Header.parse(data, is_compressed=False)
+
+        # Assert
+        self.assertIsNotNone(header)
+        self.assertEqual(header.rec_version, b"\x01\x01\x01\x01\x00")
+        self.assertEqual(header.checker, -1)
+        self.assertEqual(header.version_minor, 2)
+        self.assertEqual(header.version_major, 3)
+        self.assertEqual(header.build, 4)
+        self.assertEqual(header.timestamp, 5)
+        self.assertEqual(header.version, (6, 7))
+        self.assertEqual(header.internal_version, (8, 9))
+        self.assertEqual(header.data, b"\x0A\x0B\x0C\x0D\x0E\x0F")
+
+    def test_pack_should_return_compressed_data_which_can_be_parsed_again(self):
+        # Arrange
+        data = get_header_uncompressed()
+        header = Header.parse(data, is_compressed=False)
+
+        # Act
+        packed = header.pack()
+
+        # Assert
+        unpacked = Header.parse(packed, is_compressed=True)
+        self.assertIsNotNone(packed)
+        self.assertEqual(header, unpacked)
--- test/sample_data.py
+++ test/sample_data.py
@@ -6,3 +6,17 @@ def get_header_compressed() -> bytes:
 def get_header_uncompressed() -> bytes:
     with open("test/files/header_uncompressed.bin", "rb") as file:
         return file.read()
+
+
+def get_header_fabricated() -> bytes:
+    rec_version = b"\x01\x01\x01\x01\x00"
+    checker = b"\x00\x00\x80\xBF"  # -1
+    version_minor = b"\x02\x00"
+    version_major = b"\x03\x00"
+    build = b"\x04\x00\x00\x00"
+    timestamp = b"\x05\x00\x00\x00"
+    version = b"\x06\x00\x07\x00"
+    internal_version = b"\x08\x00\x09\x00"
+    data = b"\x0A\x0B\x0C\x0D\x0E\x0F"
+
+    return rec_version + checker + version_minor + version_major + build + timestamp + version + internal_version + data
